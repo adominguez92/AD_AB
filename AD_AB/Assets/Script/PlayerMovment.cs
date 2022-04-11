a60909dafc9a5519c2691ef2aa3d5138b444e17a
Agustin Dominguez <agndoz@gmail.com>
2022-04-11 01:26:35 -0300

    add animation+player
    Se agrego player y movment, se comienzan mecánica de stamina

--- AD_AB/Assets/Script/PlayerMovment.cs
+++ AD_AB/Assets/Script/PlayerMovment.cs
@@ -11,21 +11,40 @@ public class PlayerMovment : MonoBehaviour
     public float groundDistance = 0.4f;
 
     //variables de movimiento publicas
-    public float spped = 12f;
+    public float speed = 12f;
+    public float currentSpeed;
     public float gravity = -9.81f;
+
+    //variables de timer
+    public float coolDownRun = 3f;
+    float currentCoolDownRun;
+
+    public float coolDownRunRecobery = 5f;
+    float currentColDownRunRecobery;
+
+
 
     //variables privadas
     Vector3 velocity;
     bool isGrounded;
+    
+    void Start()
+    {
+        currentSpeed = speed;
+        currentCoolDownRun = coolDownRun;
+        currentColDownRunRecobery = coolDownRunRecobery;
+    }
 
 
     void Update()
     {
         isGrounded = IsGrounded();
+        
 
         Move();
+        Run();
 
-        if(isGrounded && velocity.y < 0)
+        if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2;
         }
@@ -38,10 +57,39 @@ public class PlayerMovment : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+        
+
         Vector3 move = transform.right * x + transform.forward * z;
 
-        cc.Move(move * spped * Time.deltaTime);
+        cc.Move(move * currentSpeed * Time.deltaTime);
     }
+
+    void Run() 
+    {
+        bool runPressed = Input.GetKey(KeyCode.LeftShift);
+
+
+
+        if (runPressed )
+        {
+            currentSpeed += Time.deltaTime;
+            
+        }
+        else
+        {
+            currentSpeed = speed;
+        }
+
+        if (currentSpeed >= speed + 4)
+        {
+            currentSpeed = speed + 4;
+        }
+    }
+
+
+    
+
+
 
     void Gravity() 
     {
